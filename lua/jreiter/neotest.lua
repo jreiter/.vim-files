96c51451d4b0b99f192ca040632b04d8ed4398a0
Johnathan Reiter <johnathanreiter@Johnathans-MacBook-Pro.local>
2023-02-06 11:21:07 -0500

    Fix for snapshot dirs breaking neotest-jest

--- lua/jreiter/neotest.lua
+++ lua/jreiter/neotest.lua
@@ -1,10 +1,15 @@
 local neotest = require('neotest')
 
+local neotest_jest = require('neotest-jest')({
+  jestCommand = 'npm test --'
+})
+neotest_jest.filter_dir = function (name)
+  return name ~= 'node_modules' and name ~= '__snapshots__'
+end
+
 neotest.setup({
   adapters = {
-    require('neotest-jest')({
-      jestCommand = 'npm test --',
-    }),
+    neotest_jest,
     require('neotest-rspec')({
       rspec_cmd = function()
         return vim.tbl_flatten({
