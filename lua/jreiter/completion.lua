5e90154a96cfda2cbd0f9ccbe4d7cd9bf059342f
jreiter <johnreiter@gmail.com>
2024-06-25 16:17:57 -0400

    Switch to eslint LSP instead of eslint_d + null_ls

--- lua/jreiter/completion.lua
+++ lua/jreiter/completion.lua
@@ -1,10 +1,6 @@
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local feedkey = function(key, mode)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
 local luasnip = require('luasnip')
--- lua/jreiter/lsp/init.lua
+++ lua/jreiter/lsp/init.lua
@@ -1,5 +1,4 @@
 require('jreiter.lsp.main')
-require('jreiter.lsp.diagnostics')
 require('jreiter.lsp.signatures')
 require('jreiter.lsp.symbols')
 require('jreiter.lsp.trouble')
