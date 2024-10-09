2750bb98b4919c53b2e17b458792fb9f2385a4e5
jreiter <johnreiter@gmail.com>
2024-10-09 11:33:06 -0400

    Use markview for markdown

--- lua/jreiter/plugins/markdown.lua
+++ lua/jreiter/plugins/markdown.lua
@@ -5,5 +5,15 @@ return {
       width_ratio = 0.8,
       height_ratio = 0.9
     }
+  },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,      -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
+    }
   }
 }
