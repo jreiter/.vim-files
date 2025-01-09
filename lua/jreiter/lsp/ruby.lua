56b59cfc0806aa9d85a26f2493bd9dd218e4ebda
jreiter <johnreiter@gmail.com>
2025-01-09 17:56:24 -0500

    Add jsonls, bye solargraph 😢
    Update to correct branch of CopilotChat
    
    fix me
    
    wip

--- lua/jreiter/lsp/ruby.lua
+++ lua/jreiter/lsp/ruby.lua
@@ -2,29 +2,6 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local lspconfig = require("lspconfig")
 
-lspconfig.solargraph.setup{
-  capabilities = capabilities,
-  filetypes = { "ruby" },
-  settings = {
-    solargraph = {
-      diagnostics = false,
-      formatting = false,
-      folding = true,
-      checkGemVersion = false,
-      useBundler = true,
-      bundlePath = vim.fn.expand("~/.rbenv/shims/bundle"),
-    },
-  },
-  on_attach = function()
-    vim.cmd([[
-    augroup LspFormatting
-      autocmd! * <buffer>
-      autocmd BufWritePre <buffer> lua vim.lsp.buf.format()
-    augroup END
-    ]])
-  end,
-}
-
 lspconfig.rubocop.setup{
   capabilities = capabilities,
   filetypes = { "ruby" },
@@ -39,6 +16,11 @@ lspconfig.rubocop.setup{
 }
 
 lspconfig.sorbet.setup{
+  capabilities = capabilities,
+  filetypes = { "ruby" },
+}
+
+lspconfig.ruby_lsp.setup{
   capabilities = capabilities,
   filetypes = { "ruby" },
 }
