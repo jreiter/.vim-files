68db8174c42e4eea266a945e5825e29d114a3748
jreiter <johnreiter@gmail.com>
2025-01-28 11:39:52 -0500

    Add conform.nvim

---
+++ lua/jreiter/plugins/formatters.lua
@@ -0,0 +1,19 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		})
+	end,
+}
