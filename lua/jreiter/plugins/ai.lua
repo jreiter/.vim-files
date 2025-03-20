353a11e75548a7f038e4ce24042d1e6a82542020
jreiter <johnreiter@gmail.com>
2025-03-20 16:44:38 -0400

    Remove CopilitChat plugin
    Accessing chat via codecompanion now

--- lua/jreiter/plugins/ai.lua
+++ lua/jreiter/plugins/ai.lua
@@ -1,19 +1,5 @@
 return {
 	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		branch = "main",
-		dependencies = {
-			{ "github/copilot.vim" }, -- or github/copilot.vim
-			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-		},
-		build = "make tiktoken", -- Only on MacOS or Linux
-		opts = {
-			debug = true, -- Enable debugging
-			-- See Configuration section for rest
-		},
-		-- See Commands section for default commands if you want to lazy load on them
-	},
-	{
 		"github/copilot.vim",
 	},
 	{
