06ba2f452f2e26cebc483d5ab8e32a3f92fd021a
jreiter <johnreiter@gmail.com>
2025-11-26 17:21:36 -0500

    Try switching to blink.cmp

--- lua/jreiter/lsp/ruby.lua
+++ lua/jreiter/lsp/ruby.lua
@@ -1,4 +1,4 @@
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 vim.lsp.config.sorbet = {
 	filetypes = { "ruby" },
--- lua/jreiter/plugins/completion.lua
+++ lua/jreiter/plugins/completion.lua
@@ -1,192 +1,73 @@
 return {
-  {
-    "L3MON4D3/LuaSnip",
-    lazy = true,
-    build = "make install_jsregexp",
-    dependencies = {
-      {
-        "rafamadriz/friendly-snippets",
-        config = function()
-          require("luasnip.loaders.from_vscode").lazy_load()
-        end,
-      },
-      {
-        "nvim-cmp",
-        dependencies = {
-          "saadparwaiz1/cmp_luasnip",
-        },
-        opts = function(_, opts)
-          opts.snippet = {
-            expand = function(args)
-              require("luasnip").lsp_expand(args.body)
-            end,
-          }
-        end,
-      },
-    },
-    config = function()
-      local luasnip = require("luasnip")
-      luasnip.filetype_extend("ruby", { "rails" })
-      luasnip.filetype_extend("typescript", { "tsdoc", "react-ts", "javascript" })
-      luasnip.filetype_extend("typescriptreact", { "tsdoc", "react-ts", "javascript" })
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    version = false, -- last release is way too old
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lsp-signature-help",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-calc",
-      "hrsh7th/cmp-path",
-      "rcarriga/cmp-dap",
-      "hrsh7th/cmp-cmdline",
-    },
-    opts = function()
-      local luasnip = require("luasnip")
-      local cmp = require("cmp")
-
-      -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#vim-vsnip
-      local has_words_before = function()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local kind_icons = {
-        Text = "",
-        Method = "",
-        Function = "󰊕",
-        Constructor = "",
-        Field = "",
-        Variable = "󰫧",
-        Class = "",
-        Interface = "",
-        Module = "",
-        Property = "",
-        Unit = "",
-        Value = "",
-        Enum = "",
-        Keyword = "",
-        Snippet = "",
-        Color = "",
-        File = "",
-        Reference = "",
-        Folder = "",
-        EnumMember = "",
-        Constant = "",
-        Struct = "",
-        Event = "",
-        Operator = "",
-        TypeParameter = "",
-      }
-
-      return {
-        formatting = {
-          format = function(entry, vim_item)
-            -- Kind icons
-            vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
-            -- Source
-            vim_item.menu = ({
-              nvim_lsp = "[LSP]",
-              vsnip = "[Vsnip]",
-              buffer = "[Buffer]",
-              calc = "[Calc]",
-              path = "[Path]",
-            })[entry.source.name]
-            return vim_item
-          end,
-        },
-        preselect = cmp.PreselectMode.None,
-        completion = {
-          completeopt = "menu,menuone,noselect", -- for preselect use "menu,menuone,noinsert"
-        },
-        mapping = {
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        },
-        snippet = {
-          -- REQUIRED - you must specify a snippet engine
-          expand = function(args)
-            luasnip.lsp_expand(args.body) -- For `luasnip` users.
-          end,
-        },
-        sources = {
-          { name = "luasnip" },
-          { name = "nvim_lsp" },
-          {
-            name = "buffer",
-            option = {
-              get_bufnrs = function()
-                -- https://github.com/hrsh7th/cmp-buffer#all-buffers
-                return vim.api.nvim_list_bufs()
-              end,
-            },
-          },
-          { name = "omni" }, -- for cucumber_language_server
-          { name = "calc" },
-          { name = "path" },
-        },
-        -- https://github.com/rcarriga/cmp-dap
-        enabled = function()
-          return vim.api.nvim_get_option_value("buftype", {}) ~= "prompt" or require("cmp_dap").is_dap_buffer()
-        end,
-      }
-    end,
-    config = function(_, opts)
-      local cmp = require("cmp")
-      cmp.setup(opts)
-
-      -- https://github.com/rcarriga/cmp-dap
-      cmp.setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
-        sources = {
-          { name = "dap" },
-        },
-      })
-
-      -- https://github.com/hrsh7th/cmp-cmdline
-      cmp.setup.cmdline("/", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
-        },
-      })
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources(
-          {
-            { name = "path" },
-          },
-          {
-            {
-              name = "cmdline",
-              option = {
-                ignore_cmds = { "Man", "!" },
-              },
-            },
-          }
-        ),
-      })
-    end,
-  },
+	{
+		"L3MON4D3/LuaSnip",
+		lazy = true,
+		build = "make install_jsregexp",
+		dependencies = {
+			{
+				"rafamadriz/friendly-snippets",
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+				end,
+			},
+		},
+		config = function()
+			local luasnip = require("luasnip")
+			luasnip.filetype_extend("ruby", { "rails" })
+			luasnip.filetype_extend("typescript", { "tsdoc", "react-ts", "javascript" })
+			luasnip.filetype_extend("typescriptreact", { "tsdoc", "react-ts", "javascript" })
+		end,
+	},
+	{
+		"saghen/blink.cmp",
+		version = "*",
+		event = "InsertEnter",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			{
+				"L3MON4D3/LuaSnip",
+				version = "v2.*",
+			},
+		},
+		opts = {
+			keymap = {
+				preset = "default",
+				["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+				["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+				["<CR>"] = { "accept", "fallback" },
+			},
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+			completion = {
+				menu = {
+					auto_show = true,
+					draw = {
+						columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+					},
+				},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 200,
+					window = {
+						border = "rounded",
+					},
+				},
+			},
+			signature = {
+				enabled = true,
+				window = {
+					border = "rounded",
+				},
+			},
+			snippets = {
+				preset = "luasnip",
+			},
+		},
+		opts_extend = { "sources.default" },
+	},
 }
