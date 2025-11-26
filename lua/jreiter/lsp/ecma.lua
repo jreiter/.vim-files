4a939ac2b43447395b71db8c6f99a4cad1789b56
jreiter <johnreiter@gmail.com>
2025-11-26 18:05:05 -0500

    Disable LSPs in codecompanion ACP diffs

--- lua/jreiter/lsp/ecma.lua
+++ lua/jreiter/lsp/ecma.lua
@@ -1,6 +1,23 @@
 local api = require("typescript-tools.api")
 
 require("typescript-tools").setup({
+	root_dir = function(fname_or_bufnr)
+		-- Handle both filename string and buffer number
+		local fname
+		if type(fname_or_bufnr) == "number" then
+			fname = vim.api.nvim_buf_get_name(fname_or_bufnr)
+		else
+			fname = fname_or_bufnr
+		end
+
+		-- Don't attach LSP to diff buffers
+		if fname:match("_diff_") then
+			return nil
+		end
+
+		-- Use default root_dir detection for normal files
+		return require("typescript-tools.config").default_config.root_dir(fname)
+	end,
 	on_attach = function(client, bufnr)
 		client.server_capabilities.documentFormattingProvider = false
 		client.server_capabilities.documentRangeFormattingProvider = false
@@ -52,6 +69,34 @@ vim.lsp.config.eslint = {
 		"eslint.config.cjs",
 		"package.json",
 	},
+	root_dir = function(fname_or_bufnr)
+		-- Handle both filename string and buffer number
+		local fname
+		if type(fname_or_bufnr) == "number" then
+			fname = vim.api.nvim_buf_get_name(fname_or_bufnr)
+		else
+			fname = fname_or_bufnr
+		end
+
+		-- Don't attach LSP to diff buffers
+		if fname:match("_diff_") then
+			return nil
+		end
+
+		-- Use default root_dir detection for normal files
+		return vim.fs.root(fname, {
+			".eslintrc",
+			".eslintrc.js",
+			".eslintrc.cjs",
+			".eslintrc.yaml",
+			".eslintrc.yml",
+			".eslintrc.json",
+			"eslint.config.js",
+			"eslint.config.mjs",
+			"eslint.config.cjs",
+			"package.json",
+		})
+	end,
 }
 
 vim.lsp.enable("eslint")
