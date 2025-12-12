2f0224955b6695e163165b1f9d185b1d56c0b465
jreiter <johnreiter@gmail.com>
2025-12-12 10:28:28 -0500

    Add golangci-lint LSP

--- lua/jreiter/lsp/go.lua
+++ lua/jreiter/lsp/go.lua
@@ -1,2 +1,19 @@
 require("go").setup({})
 vim.lsp.enable("gopls")
+
+-- Configure golangci-lint-ls with v2.x command format
+vim.lsp.config("golangci_lint_ls", {
+	cmd = { "golangci-lint-langserver", "-debug" },
+	filetypes = { "go", "gomod" },
+	root_markers = { "go.mod", ".golangci.yml", ".git" },
+	init_options = {
+		command = {
+			"golangci-lint",
+			"run",
+			"--output.json.path=stdout",
+			"--show-stats=false",
+			"--issues-exit-code=1",
+		},
+	},
+})
+vim.lsp.enable("golangci_lint_ls")
