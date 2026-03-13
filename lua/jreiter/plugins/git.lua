6b9ab9a180d3abf2fe96a37b1e08498290fc9dfc
jreiter <johnreiter@gmail.com>
2026-03-13 17:26:49 -0400

    Test out codediff.nvim

--- lua/jreiter/plugins/git.lua
+++ lua/jreiter/plugins/git.lua
@@ -1,26 +1,29 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  {
-    "sindrets/diffview.nvim",
-    opts = {
-      enhanced_diff_hl = true,
-      use_icons = true,
-      file_history_panel = {
-        log_options = {
-          git = {
-            single_file = {
-              follow = true
-            }
-          }
-        }
-      }
-    }
-  },
-  "tpope/vim-fugitive",
-  "tpope/vim-rhubarb",
-  {
-    "pwntester/octo.nvim",
-    opts = { enable_builtin = true }
-  }
+	"lewis6991/gitsigns.nvim",
+	{
+		"sindrets/diffview.nvim",
+		opts = {
+			enhanced_diff_hl = true,
+			use_icons = true,
+			file_history_panel = {
+				log_options = {
+					git = {
+						single_file = {
+							follow = true,
+						},
+					},
+				},
+			},
+		},
+	},
+	{
+		"esmuellert/codediff.nvim",
+		cmd = "CodeDiff",
+	},
+	"tpope/vim-fugitive",
+	"tpope/vim-rhubarb",
+	{
+		"pwntester/octo.nvim",
+		opts = { enable_builtin = true },
+	},
 }
-
